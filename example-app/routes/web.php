11836b41391b038177229b02b79af96b5680b1a6
MauricioRi <mmelchor524@gmail.com>
2021-05-16 18:02:13 -0500

    updateexample

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RutasController;
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +15,9 @@ use App\Http\Controllers\RutasController;
 |
 */
 Route::get('/',HomeController::class);
-Route::get('/rutas', [RutasController::class,'index']);
+//Route::get('/','HomeController'); para laravel 7
+Route::get('/login/{var}', [LoginController::class,'login']);
+//Route::get('/rutas', 'RutasController@index'); para laravel 7 7
 Route::get('/rutas/create', [RutasController::class,'create']);
 Route::get('/rutas/{idruta}', [RutasController::class,'show']);
 
