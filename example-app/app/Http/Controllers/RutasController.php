3d3afd79a39932be291ed2a132fb22fafca1e0cf
MauricioRi <mmelchor524@gmail.com>
2021-05-16 16:42:14 -0500

    rutas

--- example-app/app/Http/Controllers/RutasController.php
+++ example-app/app/Http/Controllers/RutasController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class RutasController extends Controller
 {
-    //
+public function index(){
+    return 'index';
+} 
+public function create(){
+    return 'creacion';
+}
+public function show ($curso){
+
+}
 }
