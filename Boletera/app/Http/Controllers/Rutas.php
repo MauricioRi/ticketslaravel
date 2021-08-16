f4d4478c990438850d12951f6e28ad084edab29a
MauricioRi <mmelchor524@gmail.com>
2021-08-15 23:57:49 -0500

    avanze components and reports

--- Boletera/app/Http/Controllers/Rutas.php
+++ Boletera/app/Http/Controllers/Rutas.php
@@ -2,36 +2,91 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rutas as ModelsRutas;
 use Illuminate\Http\Request;
-// use App\Models\Geocercas as ModelsGeocercas;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use App\Models\routes;
+
 class Rutas extends Controller
 {
-    // return view("components.geocercas");
+    public function __construct()
+    {
+        parent::__construct();
+        $this->middleware('auth');
+    }
 
+    public function listar(Request $request)
+    {
+        Log::debug($request);
 
-    public function listar()
-  {
-    //  compact("route"); es igual a ["ruta"=>$ruta]
-    // return  view('login.login',compact("ruta"));
-    $route = routes::paginate();
-    // echo $route; 
-    // echo "123";
-        Log::debug($route);
-        // return view("components.geocercas");
-    return  view('components.ruta', ["ruta" => $route]);
+        // Get permisions
+        $permissions = $this->getPermissions($request);
+        Log::debug($permissions);
+        session(['permissions' => $permissions]);
 
+        // Check permission and auth
+        $this->initialChecks($request, 'consulta');
 
-    // $rutas=$route;
-    // // echo $rutas;
-    // return $rutas;
+        //Obtener datos
+      //  $datos = ModelsRutas::where([['empresa', '=', Auth::user()->empresa]])->get();
+        
+        $datos = routes::paginate();
+        Log::alert($datos);
+        Log::debug($datos);
+        return  view('ruta', ["ruta" => $route]);
+        // Prepare dataSet for dataTable
+        $dataSet = array();
+        foreach ($datos as $d) {
+            $ds = array($d->id, $d->nombre);
+            $dataSet[] = $ds;
+        }
 
+        // Set for edit
+        $this->results->objPorEditar = null;
+        $this->results->dataSet = json_encode($dataSet);
 
+        return view("components.listarRutas");
+    }
 
-    //return 'login';
-  }
-    
-}
+    private function initialChecks($request, $permission)
+    {
+        $this->checkAuth($request);
+
+        // Check permissions
+        $permissions = session()->get('permissions');
+        if (empty($permissions[$permission])) {
+            return redirect()->back()->withErrors(['No tiene el permiso para completar la operación']);
+        }
+    }
+
+    public function actionsGeofences(Request $request, $action, $id) {
+        if($action == 'delete') {
+            $this->initialChecks($request, 'baja');
+
+            ModelsRutas::findOrFail($id)->delete();
+            //User::findOrFail($id)->delete();
+        } else {
+            $this->initialChecks($request, 'modificacion');
+
+            $this->results->objPorEditar = ModelsRutas::findOrFail($id);
+        }
+        
+        // Get obj
+        $obj = ModelsRutas::where([['id', '=', $id]])
+            ->get();
+        
+        // Prepare dataSet for dataTable
+        $dataSet = array();
+        foreach ($obj as $u) {
+            $ds = array($u->id, $u->nombre, $u->datos, $u->empresa);
+            $dataSet[] = $ds;
+        }
+
+        $this->results->dataSet = json_encode($dataSet);
+
+        $data = ModelsRutas::where([['id', '=', $id]])->get()[0];
+        Log::debug($data);
+        return view("components.editar-geocercas")->with(array('geofences' => $data));
+        //return response()->json(['code' => 200]);
+    }
--- Boletera/resources/views/components/listar.blade.php
+++ Boletera/resources/views/components/listar.blade.php
@@ -1,3 +1,80 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+@extends('layouts/contentLayoutMaster')
+
+@section('title', 'Rutas')
+
+@section('content')
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <table id="resultsDataTable" actionsUrl="{{ route('acciones-geocerca') }}" class="table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nombre</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
+
+@isset($results->dataSet)
+    @include('panels/dataTable')
+@endisset
+
+@endsection
+{{-- @extends('layouts.app')
+
+@section('content')
+<script src="{{ asset('js/funcionesRutas.js') }}"></script>
+    <div class="wrapper">
+
+        <div class="main-panel">
+            <h1>Listar Rutas </h1>
+            <a href="{{ route('crear_Rutas') }}">crear ruta </a>
+
+            <table class="default" border="1">
+                <tr>
+                    <td>ID RUTA</td>
+
+                    <td>NOMBRE DE RUTA</td>
+                    <td>DESCRIPCIÓN</td>
+                    {{-- <td>NUM PUNTOS</td> --}}
+                    <td>ACCIÓN</td>
+
+                </tr>
+                @foreach ($ruta as $rutas)
+                    <tr>
+                        <td>{{ $rutas->id }}</td>
+                        <td> {{ $rutas->Name_route }}</td>
+                        <td>{{ $rutas->description }}</td>
+                        {{-- <td>{{ $rutas->number_points }}</td> --}}
+
+                        <td><a href="{{ route('editar_Rutas', $rutas->id) }}">Editar_Costos</a><br>
+                            {{-- <a href="{{ route('editar_Rutas', $rutas->id) }}">Eliminar ruta</a> --}}
+                        </td>
+                        
+
+
+
+
+                    </tr>
+
+                @endforeach
+
+            </table>
+            {{ $ruta->links() }}
+
+        </div>
+    </div>
+
+
+
+
+
+
+@endsection --}}
